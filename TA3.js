adf1213c66e4405f0571d1d8d222e969392af9b7
Bruno <brunitodito@gmail.com>
2024-08-19 09:27:35 -0300

    Cambios

---
+++ TA3.js
@@ -0,0 +1,9 @@
+function removeFromArray (arreglo, item) {
+    arreglo.pop(item)
+    return arreglo
+}
+
+const Bruno = ['B','r','u','n','o']
+const o = ['o']
+console.log(removeFromArray(Bruno,o))
+
